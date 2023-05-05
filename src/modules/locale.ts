79fe82f83760b3ebc20c7c92584f8f458191fd42
Nuurrii <132632749+Nuurrii@users.noreply.github.com>
2023-05-05 05:14:08 +0200

    Initial commit

---
+++ src/modules/locale.ts
@@ -0,0 +1,21 @@
+import { config } from "../../package.json";
+
+export function initLocale() {
+  addon.data.locale = {
+    stringBundle: Components.classes["@mozilla.org/intl/stringbundle;1"]
+      .getService(Components.interfaces.nsIStringBundleService)
+      .createBundle(`chrome://${config.addonRef}/locale/addon.properties`),
+  };
+}
+
+export function getString(localString: string, noReload = false): string {
+  try {
+    return addon.data.locale?.stringBundle.GetStringFromName(localString);
+  } catch (e) {
+    if (!noReload) {
+      initLocale();
+      return getString(localString, true);
+    }
+    return localString;
+  }
+}
